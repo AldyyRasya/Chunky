43b93874ddf7e6c563f57ee8a6e23dc22fee472b
AldyyRasya <138641328+AldyyRasya@users.noreply.github.com>
2023-07-05 10:26:59 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6285913856428'] //ur owner number
+global.ownernomer = "6285913856428" //ur owner number2
+global.ownername = "My Owner" //ur owner name
+global.ytname = "-" //ur yt chanel name
+global.socialm = "-" //ur github or insta name
+global.location = "Indonesia, Yogyakarta, In your Heart" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Chunky Bot"
+global.ownernumber = '6285913856428'
+global.ownername = 'My Owner'
+global.ownerNumber = ["6285913856428@s.whatsapp.net"]
+global.ownerweb = "-"
+global.websitex = "-"
+global.wagc = "-"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "Chunky Bot Inc."
+global.botscript = '-' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.author = "Author\n\n+6285913856428"
+global.creator = "6285913856428@s.whatsapp.net"
+global.prefa = ['/','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
 
